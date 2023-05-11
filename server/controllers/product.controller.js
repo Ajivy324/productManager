d5e934fba6a770febdc4bb5cd144b2dcf00cdbbb
Andrew Ivy <ajivy324@yahoo.com>
2023-05-11 07:46:09 -0500

    add display on see client repo

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -16,10 +16,18 @@ module.exports.createProduct = (request, response) => {
 module.exports.findAllProducts = (req, res) => {
     Product.find()
         .then((allProducts) => {
-            res.json({ products: allProducts })
+            res.json(allProducts)
         })
         .catch((err) => {
             res.json({ message: 'Something is not right', error: err })
         });
 }
+
+module.exports.getProduct = (req, res) => {
+    Product.findOne({_id:req.params.id})
+        .then(product => res.json(product))
+        .catch(err => res.json(err));
+}
+
+
 
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -5,5 +5,8 @@ module.exports = (app) => {
     app.get('/api', ProductController.index);
     app.get('/api/products', ProductController.findAllProducts);
     app.post('/api/products', ProductController.createProduct);
+    app.get('/api/products/:id', ProductController.getProduct);
+
+
 }
 
